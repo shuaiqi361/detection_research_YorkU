6f0ca9b1c1f929835697396ba13096a2672aab79
shuaiqi361 <liukeyi1993@gmail.com>
2020-04-04 19:38:04 -0400

    add model init

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,14 @@
 from .VGG_SSD import SSD512, MultiBoxLoss
+from .VGG_SSD2 import SSD512 as SSD_2
+from .VGG_SSD2 import MultiBoxLoss as MBL2
 
 
 def model_entry(config):
     if config.model['arch'].upper() == 'VGG_SSD':
         print('Loading SSD with VGG16 backbone ......')
         return SSD512(config['n_classes'], device=config.device), MultiBoxLoss
+    elif config.model['arch'].upper() == 'VGG_SSD2':
+        print('Loading SSD2 with VGG16 backbone ......')
+        return SSD_2(config['n_classes'], device=config.device), MBL2
     else:
         raise NotImplementedError
